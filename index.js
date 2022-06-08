b0cbdf68713ef8f5ddcafcf72caea96ab6ffa330
Terell <Terelld@gmail.com>
2022-06-08 07:44:20 -0500

    commit tree 912f8964d95d5e75cfe68108bb9136a791acce90

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+var element = document.getElementById("main");
+element.parentNode.removeChild(element);
+
+let newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Terell is the champion';
+
+// const h1 = <h1>victory</h1>
+
+// newHeader.innerHTML = "<h1>victory</h1>";
